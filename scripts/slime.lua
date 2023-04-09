446724e871bb6d48f1fc8ac2ceb2b66fd5063dbb
EwDev <74792428+Ew-Developer@users.noreply.github.com>
2023-04-09 21:48:27 +0200

    Update slime.lua

--- scripts/slime.lua
+++ scripts/slime.lua
@@ -1,2 +1,2 @@
--- moved to my cdn
+warn("slime.lua has been moved to my cdn, please use 'https://cdn.ewdev.net/scripts/slime.lua' instead")
 loadstring(game:GetService("HttpService"):GetAsync("https://cdn.ewdev.net/scripts/slime.lua"))()
