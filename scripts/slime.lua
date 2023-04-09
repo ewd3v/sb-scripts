312013131f7178a38065d73d42287ca1676ba34e
EwDev <74792428+Ew-Developer@users.noreply.github.com>
2023-04-09 21:49:02 +0200

    Create slime.lua

--- scripts/slime.lua
+++ scripts/slime.lua
@@ -1,2 +1,2 @@
-warn("slime.lua has been moved to my cdn, please use 'https://cdn.ewdev.net/scripts/slime.lua' instead")
+warn("slime.lua has been moved to my cdn, please use 'https://cdn.ewdev.net/scripts/slime.lua' instead.")
 loadstring(game:GetService("HttpService"):GetAsync("https://cdn.ewdev.net/scripts/slime.lua"))()
